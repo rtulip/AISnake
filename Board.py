367b13d3fb22a10950794334b254cd0ee73275fd
rtulip <robert.tulip97@gmail.com>
2018-04-17 23:57:40 -0700

    Added Food indexing

--- Board.py
+++ Board.py
@@ -28,14 +28,15 @@ class board:
     
     def draw(self):
         self.__grid = np.zeros((BOARD_SIZE,BOARD_SIZE))
+        if self.__food:
+            self.__grid[self.__food[1]][self.__food[0]] = 4
         if self.__snake1:
             for xy in self.__snake1.get_location():
                 self.__grid[xy[1]][xy[0]] = 1
         if self.__snake2:
             for xy in self.__snake2.get_location():
-                self.__grid[xy[0]][xy[1]] = 2
-        if self.__food:
-            self.__grid[self.__food[0]][self.__food[1]] = 4
+                self.__grid[xy[1]][xy[0]] = 2
+        
     
     def update(self, snake1_dir = None, snake2_dir = None, new_food = False):
         if snake1_dir:
@@ -50,14 +51,26 @@ class board:
         if self.__snake2:
             self.__snake2.move()
         
-        self.draw()     
+        self.draw()
+        
+    def grow_snake(self,snake_id):
+        if snake_id == 1:
+            self.__snake1.grow()
+        elif snake_id == 2:
+            self.__snake2.grow()
+        else:
+            print("invalid Snake_id")
+         
         
 s = snake()
-b = board(s)
-"""
+f = food(4,2)
+b = board(s,food = f)
+
 print(b)
 b.update()
 print(b)
 b.update(snake1_dir=2)
 print(b)
-"""
+b.grow_snake(1)
+b.update(new_food = food(7,7))
+print(b)
--- Food.py
+++ Food.py
@@ -14,6 +14,14 @@ class food:
     def __str__(self):
         return str(self.get_location())
     
+    def __getitem__(self,idx):
+        if idx == 0:
+            return self.__x
+        elif idx == 1:
+            return self.__y
+        else:
+            raise IndexOutOfRangeException
+    
     def get_location(self):
         return [self.__x,self.__y]
     
